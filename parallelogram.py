8a644cc326494090af330124f0264e535f1a4b33
oklabsfullstackmay2023 <oklabsfullstackmay2023@gmail.com>
2023-12-06 17:08:07 +0530

    hii

---
+++ parallelogram.py
@@ -0,0 +1,9 @@
+b =input('Please enter the base of the parallelogram ')
+b =float(b)
+
+h =input("Please enter the height of the parallelogram ")
+h =float(h)
+
+p = 2*(b+h)
+
+print(f'The perimeter of the parallelogram {p} ')
